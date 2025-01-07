f34f9bae3606716afd2d7b6ee0b56161d65c8f8e
Thiago <thiago.geremias@compasso.com.br>
2025-01-07 11:08:32 -0300

    Fix desafio 4

--- desafio-4/verify.sh
+++ desafio-4/verify.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-kubectl get pods -l app=sales -o jsonpath='{.items[0].status.containerStatuses[0].restartCount}' | grep -q '^[1-9]' && exit 1 || exit 0
+kubectl get pods -l app=sales -o jsonpath='{.items[0].status.containerStatuses[0].restartCount}' -n store-dev | grep -q '^[1-9]' && exit 1 || exit 0
