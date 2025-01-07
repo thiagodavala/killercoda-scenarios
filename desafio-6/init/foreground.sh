26a674ba223a00add7e197adff32c7a7bd8ee1ed
Thiago <thiago.geremias@compasso.com.br>
2025-01-07 18:28:53 -0300

    Add Desafio 6

--- desafio-6/init/foreground.sh
+++ desafio-6/init/foreground.sh
@@ -1,1 +1,7 @@
 FILE=/ks/wait-init.sh; while ! test -f ${FILE}; do clear; sleep 0.1; done; bash ${FILE}
+
+NAMESPACE="kube-system"
+PODS=$(kubectl get pods -n $NAMESPACE -l k8s-app=kube-dns -o jsonpath='{.items[*].metadata.name}')
+for POD in $PODS; do
+  kubectl exec -it $POD -n $NAMESPACE -- tc qdisc add dev eth0 root netem loss 50%
+done
