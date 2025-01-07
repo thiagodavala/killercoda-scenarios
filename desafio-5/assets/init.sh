987ea1617921554e92569e715b4f0cf6594e9fe8
Thiago <thiago.geremias@compasso.com.br>
2025-01-07 15:53:13 -0300

    Add Desafio 5

--- desafio-5/assets/init.sh
+++ desafio-5/assets/init.sh
@@ -12,6 +12,8 @@ EOT
 
 # scenario specific
 kubectl -f /ks/init.yaml create
+echo 172.30.1.2 world.universe.mine >> /etc/hosts
+kubectl -n ingress-nginx wait --for=condition=available --timeout=600s deployment/ingress-nginx-controller
 
 # mark init finished
 touch /ks/.initfinished
