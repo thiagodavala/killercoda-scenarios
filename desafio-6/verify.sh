24fbff1402eef015a734b3741a9ca24e3983dca2
Thiago <thiago.geremias@compasso.com.br>
2025-01-17 11:14:56 -0300

    Fix Scenario 6

--- desafio-6/verify.sh
+++ desafio-6/verify.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sleep 10s
-kubectl get pods -l app=sales -o jsonpath='{.items[0].status.containerStatuses[0].restartCount}' -n store-dev | grep -q '^[1-9]' && exit 1 || exit 0
+kubectl get pods -l app=nginx -o jsonpath='{.items[0].status.containerStatuses[0].restartCount}' -n xpto | grep -q '^[1-9]' && exit 1 || exit 0
