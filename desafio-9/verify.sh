897f081f073f9d448e4fef5e23e31396013e095c
Thiago <thiago.geremias@compasso.com.br>
2025-01-19 18:00:18 -0300

    Add Scenario 9

--- desafio-9/verify.sh
+++ desafio-9/verify.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-kubectl get pod -l app=rbac-initcontainer | grep Running | grep -q 'true' && exit 0 || exit 1
+kubectl get pod -l app=rbac-initcontainer | grep -v Running && exit 1 || exit 0
