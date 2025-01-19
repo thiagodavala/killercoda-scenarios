2592a20ba012a18199f83ff4cecd24e1ea23ae98
Thiago <thiago.geremias@compasso.com.br>
2025-01-19 18:34:21 -0300

    Add Scenario 9

--- desafio-9/verify.sh
+++ desafio-9/verify.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-kubectl get pods -l app=rbac-initcontainer --no-headers | awk '{if ($2 != "Running") {print "Pod " $1 " não está em Running. Status: " $2; exit 1}}' || exit 0
+kubectl get pods  -l app=rbac-initcontainer | grep Running
